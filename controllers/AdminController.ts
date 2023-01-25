ec656b0ddf1831ae26ec50d0fce15a927234dcf5
Gr8fulnez <oluwolejohn72@gmail.com>
2023-01-25 01:14:10 +0100

    feat: implement email validation and user  hash password

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,12 +1,23 @@
 import { Request, Response, NextFunction } from 'express'
 import{CreateVendorInput} from '../dto'
 import { vendor } from '../models'
+import { GeneratePassword, GenerateSalt } from '../utility'
 
 export const CreateVendor = async (req:Request, res: Response, next: NextFunction) => {
     
     const { name, address, pincode, foodType, email, password, ownerName, phone } = <CreateVendorInput>req.body
     
-   
+    const existingVendor = await vendor.findOne({ email: email })
+
+    if (existingVendor !== null) {
+        return res.json({"message": "A vendor is exists with this email ID"})
+    }
+
+    //generate a salt 
+    const salt = await GenerateSalt()
+    const userPassword = await GeneratePassword(password, salt);
+
+    //encrypt the password 
 
      const CreatedVendor = await vendor.create ({
         name: name,
@@ -16,8 +27,8 @@ export const CreateVendor = async (req:Request, res: Response, next: NextFunctio
         address: address,
         phone: phone,
         email: email,
-        password: password,
-        salt: 'hdsdnjsjds sjsnjsdnsjdns',
+        password: userPassword,
+        salt: salt,
         serviceAvailable: false,
         coverImages: [],
         rating: 0,
